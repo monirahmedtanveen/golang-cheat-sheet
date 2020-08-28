b6ca87f0d8c042a466bd24f484b002f21bc8be63
Monir Ahmed <monir.ahmed0993@gmail.com>
2020-08-28 20:26:57 +0600

    folder serial added

---
+++ 3.variables/variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a = "Initialize"
+  fmt.Println("a=", a)
+
+  var b, c int = 1, 2
+  fmt.Println("b=", b, ", c=", c)
+
+  var d = 20.56
+  fmt.Println("d=", d)
+
+  var e = true
+  fmt.Println("e=", e)
+
+  var f int
+  fmt.Println("f=", f)
+
+  var g string = "Hello Golang!!"
+  fmt.Println("g=", g)
+
+  h := "apple mac pro!!"
+  fmt.Println("h=", h)
+}
